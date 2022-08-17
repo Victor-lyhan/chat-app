57cd7c9c8a13feb689e654b4f175968f90ed0cb9
Victor Han <liyanghan1112@gmail.com>
2022-08-17 14:57:00 -0500

    Initial Commit

---
+++ chatApp/chatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatAppApp.swift
+//  chatApp
+//
+//  Created by Victor Han on 2022/8/17.
+//
+
+import SwiftUI
+
+@main
+struct chatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
